77d1e7320693a9f79e861b3f8c95a858e5acbb38
Melliti <r.melliti@protonmail.com>
2019-12-20 00:10:43 -0500

    [add] criteria class isValidLoan function added

--- criteria.py
+++ criteria.py
@@ -4,10 +4,25 @@ class Criteria:
 
     def annualInterestPayment(self, dataClient):
         self.__annualIR = float(dataClient["Requested loan amount"]) * float(dataClient["Interest rate"])
-        print(self.__annualIR)
+        # print(self.__annualIR)
         pass
     
     def loanToIncome(self, dataClient):
         self.__loanToIncome = float(dataClient["Requested loan amount"]) / float(dataClient["Income"])
-        print(self.__loanToIncome)
+        # print(self.__loanToIncome)
         pass
+
+    def isValidLoan(self, dataClient):
+        print(self.__annualIR)
+        print((20 / 100) * float(dataClient["Income"]))
+        if (self.__annualIR > (20 / 100) * float(dataClient["Income"])):
+            print("1")
+            return False
+        elif (self.__loanToIncome > 4):
+            print("2")
+            return False
+        elif (float(dataClient["Credit Score"]) < 600):
+            print("3")
+            return False
+        else:
+            return True
