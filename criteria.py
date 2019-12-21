5c470919cfd73f8b8890ebda6fd8c3b551749916
Melliti <r.melliti@protonmail.com>
2019-12-20 20:55:16 -0500

    [Add] Error management of input

--- criteria.py
+++ criteria.py
@@ -3,26 +3,19 @@ class Criteria:
     __loanToIncome = ""
 
     def annualInterestPayment(self, dataClient):
-        self.__annualIR = float(dataClient["Requested loan amount"]) * float(dataClient["Interest rate"])
-        # print(self.__annualIR)
+        self.__annualIR = float(dataClient["Requested loan amount"]) * (float(dataClient["Interest rate"]) / 100)
         pass
     
     def loanToIncome(self, dataClient):
         self.__loanToIncome = float(dataClient["Requested loan amount"]) / float(dataClient["Income"])
-        # print(self.__loanToIncome)
         pass
 
     def isValidLoan(self, dataClient):
-        print(self.__annualIR)
-        print((20 / 100) * float(dataClient["Income"]))
         if (self.__annualIR > (20 / 100) * float(dataClient["Income"])):
-            print("1")
             return False
         elif (self.__loanToIncome > 4):
-            print("2")
             return False
-        elif (float(dataClient["Credit Score"]) < 600):
-            print("3")
+        elif (float(dataClient["Credit score"]) < 600):
             return False
         else:
             return True
--- main.py
+++ main.py
@@ -1,19 +1,74 @@
+import criteria
+import sys
+# import sciter
+
 class Main:
     client =  {
-        "first name": "",
+        "First name": "",
         "Last name": "",
         "Social security number": "",
         "Requested loan amount": "",
         "Loan duration": "",
         "Interest rate": "",
         "Income": "",
-        "Credit Score": "",
+        "Credit score": "",
     }
+
+    testClient =  {
+        "first name": "Mazir",
+        "Last name": "Melliti",
+        "Social security number": "121102934",
+        "Requested loan amount": "20000",
+        "Loan duration": "24",
+        "Interest rate": "5",
+        "Income": "40000",
+        "Credit Score": "898",
+    }
+
+    def isValidName(self):
+        firstname = self.client["Social security number"].replace('-', '')
+        lastname = self.client["Last name"].replace('-', '')
+        if (len(lastname) < 2 or len(firstname) < 2):
+            raise Exception("[Input Error] firstname and lastname must have at least 2 character")
+        
+
+    def isValidSocialSecurity(self):
+        ssn = self.client["Social security number"].replace('-', '')
+        if (len(ssn) != 9):
+            raise Exception("[Input Error] Social Security number must have 9 digits")
+        if not (ssn.isnumeric()):
+            raise Exception("[Input Error] Social Security number must have 9 digits")        
+
+    def isValidValues(self):
+        for c in list(self.client)[3: len(self.client)]:
+            if not (self.client[c].isnumeric()):
+                raise Exception("[Input Error] " + c + " must contains only digits")
+
+    def isValidCreditScore(self):
+        cs = int(self.client["Credit score"])
+        if (cs < 300 or cs > 850):
+            raise Exception("[Input Error] Credit score is between 300 and 850")
+
+
     def main(self):
         for val in self.client.keys():
             self.client[val] = input(val + ": ")
-        for val in self.client.values():
-            print(val)
+
+        try:
+            self.isValidName()
+            self.isValidSocialSecurity()
+            self.isValidValues()
+            self.isValidCreditScore()
+        except Exception as error:
+            print(error)
+            exit()
 
 st = Main()
 st.main()
+cr = criteria.Criteria()
+cr.annualInterestPayment(st.client)
+cr.loanToIncome(st.testClient)
+if cr.isValidLoan(st.client):
+    print ("Refused")
+else:
+    print ("Accepted")
